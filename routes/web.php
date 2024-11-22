55aae6c16665e37e2a13a41ac1f7124d39aa61b1
rafal <orlowskirafa@gmail.com>
2024-11-22 20:09:03 +0100

    update redirects after register and login, add task  to navbar

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Route::middleware(['auth'])->group(function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
